ded89d09eae8e5855311817b25f6dbc93c6ee9e2
Gil Rutter <g.o.rutter@gmail.com>
2020-06-08 22:41:36 +0100

    Improve indentation

--- utils/tetris.hpp
+++ utils/tetris.hpp
@@ -15,66 +15,65 @@
 
 using namespace std;
 
-class Tetris {
-    public:
-        Tetris(Grid& grid) : grid{grid}, tetrisview{grid} {};
+struct Tetris {
+    Tetris(Grid& grid) : grid{grid}, tetrisview{grid} {};
 
-        void start() {
-            int highscore = 0;
-            int score;
-            do {
-                score = run();
-                highscore = score > highscore? score : highscore;
-                tetrisview.update_highscore(highscore);
-            } while(restart_input());
-        }
+    void start() {
+        int highscore = 0;
+        int score;
+        do {
+            score = run();
+            highscore = score > highscore? score : highscore;
+            tetrisview.update_highscore(highscore);
+        } while(restart_input());
+    }
 
-        bool restart_input() {
-            nodelay(stdscr, FALSE);
-            while(true) {
-                switch (getch()){
-                    case 'r':
-                        return true;
-                    case 'q':
-                        return false;
-                }
+    bool restart_input() {
+        nodelay(stdscr, FALSE);
+        while(true) {
+            switch (getch()){
+                case 'r':
+                    return true;
+                case 'q':
+                    return false;
             }
         }
+    }
 
-        int run() {
-            int score = 0;
-            grid.zero();
-            int cycle_time_ms = start_cycle_time_ms;
-            active_piece.reset();
-            tetrisview.hide_game_over();
+    int run() {
+        int score = 0;
+        grid.zero();
+        int cycle_time_ms = start_cycle_time_ms;
+        active_piece.reset();
+        tetrisview.hide_game_over();
 
-            int next_shape = Shapes::random_shape();
+        int next_shape = Shapes::random_shape();
 
-            while(true) {
-                if(!active_piece) {
-                    active_piece.emplace(Shapes::all_shapes[next_shape], grid);
-                    next_shape = Shapes::random_shape();
+        while(true) {
+            if(!active_piece) {
+                active_piece.emplace(Shapes::all_shapes[next_shape], grid);
+                next_shape = Shapes::random_shape();
 
-                    tetrisview.update_score(score, cycle_time_ms);
-                    tetrisview.update_next_shape(Shapes::all_shapes[next_shape]);
-                    
-                    if(!active_piece->valid_state()) break; // New piece being immediately invalid marks end of game.
-                }
-                active_piece->down();
+                tetrisview.update_score(score, cycle_time_ms);
+                tetrisview.update_next_shape(Shapes::all_shapes[next_shape]);
 
-                if (active_piece->landed){
-                    grid.absorb(active_piece->global_grid);
-                    active_piece.reset();
-                    score += remove_rows();
-                    cycle_time_ms *= 0.99;
-                }
-
-                tetrisview.update_grid(active_piece);
-                if (active_piece.has_value()) input_loop(cycle_time_ms);
+                if(!active_piece->valid_state()) break; // New piece being immediately invalid marks end of game.
             }
-            tetrisview.show_game_over();
-            return score;
+            active_piece->down();
+
+            if (active_piece->landed){
+                grid.absorb(active_piece->global_grid);
+                active_piece.reset();
+                score += remove_rows();
+                cycle_time_ms *= 0.99;
+            }
+
+            tetrisview.update_grid(active_piece);
+            if (active_piece.has_value()) input_loop(cycle_time_ms);
         }
+        tetrisview.show_game_over();
+        return score;
+    }
 
     int remove_rows() {
         size_t i_low, i_high;
@@ -95,42 +94,42 @@ class Tetris {
         return score;
     }
 
-        void input_loop(int cycle_time_ms) {
-            const chrono::time_point start = chrono::steady_clock::now();
-            int elapsed;
-            bool take_input = true;
-            nodelay(stdscr, TRUE);
-            keypad(stdscr, TRUE);
-            do {
-                int ch = getch();
-                switch (ch) {
-                    case 'a':
-                    case KEY_LEFT:
-                        active_piece->left();
-                        break;
-                    case 'd':
-                    case KEY_RIGHT:
-                        active_piece->right();
-                        break;
-                    case 'w':
-                    case KEY_UP:
-                        active_piece->rotate();
-                        break;
-                    case 's':
-                    case KEY_DOWN:
-                        active_piece->down();
-                        take_input = !active_piece->landed;
-                        break;
-                    case ' ':
-                        active_piece->fall();
-                        take_input = false;
-                        break;
-                }
-                if(ch != ERR) tetrisview.update_grid(active_piece);
-                chrono::time_point end = chrono::steady_clock::now();
-                elapsed = chrono::duration_cast<chrono::milliseconds>(end - start).count();
-            } while (take_input && elapsed < cycle_time_ms);
-        }
+    void input_loop(int cycle_time_ms) {
+        const chrono::time_point start = chrono::steady_clock::now();
+        int elapsed;
+        bool take_input = true;
+        nodelay(stdscr, TRUE);
+        keypad(stdscr, TRUE);
+        do {
+            int ch = getch();
+            switch (ch) {
+                case 'a':
+                case KEY_LEFT:
+                    active_piece->left();
+                    break;
+                case 'd':
+                case KEY_RIGHT:
+                    active_piece->right();
+                    break;
+                case 'w':
+                case KEY_UP:
+                    active_piece->rotate();
+                    break;
+                case 's':
+                case KEY_DOWN:
+                    active_piece->down();
+                    take_input = !active_piece->landed;
+                    break;
+                case ' ':
+                    active_piece->fall();
+                    take_input = false;
+                    break;
+            }
+            if(ch != ERR) tetrisview.update_grid(active_piece);
+            chrono::time_point end = chrono::steady_clock::now();
+            elapsed = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+        } while (take_input && elapsed < cycle_time_ms);
+    }
 
     private:
         Grid& grid;
